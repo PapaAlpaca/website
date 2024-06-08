b38f98b3e6622472ec8d121c10d7780f7b2feeee
Isaac <isaacw1925@gmail.com>
2024-06-08 14:58:16 -0700

    Theme to nav

--- website/src/components/Navbar.js
+++ website/src/components/Navbar.js
@@ -3,11 +3,27 @@ import React from 'react'
 import {Link} from "react-router-dom";
 
 function Navbar() {
+
+    const changeTheme = (event) => {
+        document.activeElement.blur();
+    }
+
     return (
         <nav className={"navbar"}>
-            <Link to={"/website"}>Home</Link>
-            <Link to={"/projects"}>Projects</Link>
-            {/*<Link to={"/"}>Resumé</Link>*/}
+            <div className={"links"}>
+                <Link to={"/website"}>Home</Link>
+                <Link to={"/projects"}>Projects</Link>
+                {/*<Link to={"/"}>Resumé</Link>*/}
+            </div>
+            <div className={"themes"}>
+                <select onChange={changeTheme}>
+                    <option>Light</option>
+                    <option>Dark</option>
+                    <option>Auto</option>
+                    <option>Celestial</option>
+                    <option>Ghibli</option>
+                </select>
+            </div>
         </nav>
     )
 }
