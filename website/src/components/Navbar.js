9bd1d5f1bc810b911ecff845d39e77f4d660f036
Isaac <isaacw1925@gmail.com>
2024-06-08 14:45:33 -0700

    link update for pages

--- website/src/components/Navbar.js
+++ website/src/components/Navbar.js
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 function Navbar() {
     return (
         <nav className={"navbar"}>
-            <Link to={"/"}>Home</Link>
+            <Link to={"/website"}>Home</Link>
             <Link to={"/projects"}>Projects</Link>
             {/*<Link to={"/"}>Resumé</Link>*/}
         </nav>
